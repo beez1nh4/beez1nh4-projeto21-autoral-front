13e557f2fa97d719c5e52f8a69598a0e9728ed6d
Melissa <melmihomel@gmail.com>
2023-04-16 20:41:48 -0300

    feat: add basic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import Enroll from './pages/Enroll';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+
+import { EventInfoProvider } from './contexts/EventInfoContext';
+import { UserProvider } from './contexts/UserContext';
+
+import useToken from './hooks/useToken';
+
 function App() {
   return (
-    <>Hocus Focus</>
+    <>
+      <ToastContainer />
+      <EventInfoProvider>
+        <UserProvider>
+          <Router>
+            <Routes>
+              <Route path="/enroll" element={<Enroll />} />
+              <Route path="/sign-in" element={<SignIn />} />
+
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRouteGuard>
+                    
+                  </ProtectedRouteGuard>
+                }
+              >
+                <Route index path="*" element={<Navigate to="/home" />} />
+              </Route>
+            </Routes>
+          </Router>
+        </UserProvider>
+      </EventInfoProvider>
+    </>
   );
 }
 
 export default App;
+
+function ProtectedRouteGuard({ children }) {
+  const token = useToken();
+
+  if (!token) {
+    return <Navigate to="/sign-in" />;
+  }
+
+  return <>{children}</>;
+}
